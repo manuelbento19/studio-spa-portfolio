c9c41c90a09db8f38eb1277c2c3c18026401df38
Manuel Canganjo <manuelbentomb.223@hotmail.com>
2023-10-05 16:32:22 +0100

    fix: Appending flex-column propertie on footer

--- src/app/components/footer/index.jsx
+++ src/app/components/footer/index.jsx
@@ -8,7 +8,7 @@ export default function Footer(){
                     Studio
                 </a>
                 <p className='max-w-2xl text-sm my-10 text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate sint corrupti perferendis quasi quos, dolores porro vel, voluptatibus inventore ratione pariatur eos sunt magni itaque eveniet molestias culpa? Doloribus, eveniet?</p>
-                <ul className='max-w-xl w-full flex tablet:flex-wrap gap-3 items-center justify-between'>
+                <ul className='max-w-xl w-full flex tablet:flex-col gap-3 items-center justify-between'>
                     <li className='flex flex-col'>
                         <a href='#' target='_blank' className='flex flex-col items-center text-[.7rem] gap-4'>
                             <FaMapMarkerAlt size={19} />
