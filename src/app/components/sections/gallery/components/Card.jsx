2205444d2683047462db399020709725496e04a2
Manuel Canganjo <manuelbentomb.223@hotmail.com>
2023-10-06 12:36:49 +0100

    feat: Lazy Loading on gallery section

--- src/app/components/sections/gallery/components/Card.jsx
+++ src/app/components/sections/gallery/components/Card.jsx
@@ -1,9 +1,22 @@
-import { memo } from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import { memo, useEffect } from "react";
 
 const Card = (props) =>{
+    const cardRef = useRef(false);
+    const [show,setShow] = useState(false)
+    useEffect(()=>{
+        const observer = new IntersectionObserver((entry)=>{
+            if(entry[0].isIntersecting)
+            setShow(true)
+        })
+        observer.observe(cardRef.current);
+        return () => observer.disconnect();
+    },[])
+    
     return(
-        <li onClick={()=>props.open(props.url)} className='col-auto rounded border overflow-hidden cursor-pointer'>
-            <img src={props.url} className='h-full w-full object-cover relative object-center shadow-xl hover:scale-110 duration-1000' alt=""/>
+        <li ref={cardRef} onClick={()=>props.open(props.url)} className='col-auto rounded border overflow-hidden cursor-pointer'>
+            {show && <img src={props.url} className='h-full w-full object-cover relative object-center shadow-xl hover:scale-110 duration-1000' alt=""/>}
         </li>
     )
 }
